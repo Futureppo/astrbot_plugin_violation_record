e91a337fdc103184768d2e08df0a522a84cc2c1e
Futureppo <luominzhi2005@qq.com>
2025-10-20 10:10:34 +0800

    Update

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ violationlist = list(violationdata.values())
 @register(
     "astrbot_plugin_violation_record",
     "Futureppo",
-    "QQ查询违规插件",
+    "QQ查询账号违规记录插件",
     "0.0.1",
     "https://github.com/Futureppo/astrbot_plugin_violation_record",
 )
